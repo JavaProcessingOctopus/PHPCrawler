03a446405217c313093a83a8a43c1fd178c01341
JavaProcessingOctopus <javaprocessingoctopus@outlook.com>
2016-09-11 12:20:17 +0200

    Added a controller who gets php.net text and dumps a map of the top words

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\DomCrawler\Crawler;
+
 class DefaultController extends Controller
 {
     /**
