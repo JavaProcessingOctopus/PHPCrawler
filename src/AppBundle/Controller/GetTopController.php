3dc0f2fb41dba3e8466ac43284119f784098c898
JavaProcessingOctopus <javaprocessingoctopus@outlook.com>
2016-09-12 09:19:22 +0200

    Refactored getTopEntiy by internalizing crawling function as methods and making crawl result an attribute. This makes code more Obj Oriented and should make ORM easier

--- src/AppBundle/Controller/GetTopController.php
+++ src/AppBundle/Controller/GetTopController.php
@@ -6,7 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use AppBundle\Entity\GetTopEntity;
@@ -32,19 +31,17 @@ class GetTopController extends Controller {
 		$form->handleRequest($request);
 	
 		if ($form->isSubmitted() && $form->isValid()) {
-			// $form->getData() holds the submitted values
-			// but, the original `$task` variable has also been updated
-			$entity = $form->getData();
-			$url = $entity->getUrl();
-			$topNumber = $entity->getTopNumber();
-	
-			// ... perform some action, such as saving the task to the database
-			// for example, if Task is a Doctrine entity, save it!
 			// $em = $this->getDoctrine()->getManager();
-			// $em->persist($task);
+			// $em->persist($entity);
 			// $em->flush();
-	
-			return $this->getTopWord($url, $topNumber);
+			
+			// Build response
+			$responseStr = "";
+			foreach ($entity->getTop() as $key => $value){
+				$responseStr .= $key." : ".$value."<br />";
+			}
+			
+			return new Response( $responseStr );
 		}
 	
 		return $this->render('getTopForm.html.twig', array(
@@ -52,33 +49,4 @@ class GetTopController extends Controller {
 		));
 	}
 	
-	private function getTopWord($url, $topNumber) {
-		//$url = "http://php.net";
-		//$topNumber = 10;
-		
-		// Step 1 - get Page
-		$crawler = new Crawler(file_get_contents($url));
-		$raw = implode($crawler/*->filter('body')*/->each(function (Crawler $node, $i) {
-			return $node->text();
-		}));
-		// Step 2 - Put all words in a word & number map
-		$map = array_count_values(
-			preg_split("/[\s,]+/", $raw)
-		);
-		// Step 2.1 - Kepp only words longer than 3
-		$map = array_filter($map, array($this, 'wordPostFilter'), ARRAY_FILTER_USE_KEY);
-		arsort( $map );
-		// Step 3 - get top 10
-		$map = array_slice ( $map, 0, $topNumber );
-		
-		// Step 4 - Write response
-		$responseStr = "";
-		foreach ($map as $key => $value){
-			$responseStr .= $key."<br />";
-		}
-		
-		return new Response($responseStr);
-	}
-	
-	function wordPostFilter($word) { return strlen($word) >3; }
 }
---
+++ src/AppBundle/Entity/GetTopEntity.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+class GetTopEntity {
+    protected $url;
+    protected $topNumber;
+    
+    protected $top;
+
+    public function getUrl() {
+        return $this->url;
+    }
+
+    public function setUrl($url) {
+        $this->url = $url;
+    }
+
+    public function getTopNumber() {
+        return $this->topNumber;
+    }
+
+    public function setTopNumber($topNumber) {
+        $this->topNumber = $topNumber;
+    }
+    
+    public function getTop() {
+    	if (! isset($this->top)) {
+    		$this->initTop();
+    	}
+    	return $this->top;
+    }
+    
+    private function initTop() {
+    	// Step 1 - Get WebPage
+    	$crawler = new Crawler(file_get_contents($this->url));
+    	$raw = implode($crawler->each(function (Crawler $node, $i) {
+    		return $node->text();
+    	}));
+    		// Step 2 - Put all words in a word & number map
+    		$map = array_count_values(
+    				preg_split("/[\s,]+/", $raw)
+    				);
+    		// Step 2.1 - Keep only words longer than 3
+    		$map = array_filter($map, array($this, 'wordPostFilter'), ARRAY_FILTER_USE_KEY);
+    		
+    		arsort( $map );
+    		// Step 3 - Get top 10 from map and set attribute
+    		$this->top = array_slice ( $map, 0, $this->topNu );
+    }
+    
+    private function wordPostFilter($word) { return strlen($word) >3; }
+}
